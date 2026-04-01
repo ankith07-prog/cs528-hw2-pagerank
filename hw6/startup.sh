1a5f32206fcf2cf5d36a4c66459298055fdc9c88
Ankith Rao <ankith07@bu.edu>
2026-04-01 22:51:16 +0000

    Read DB env vars from VM metadata in HW6 startup script

--- hw6/startup.sh
+++ hw6/startup.sh
@@ -2,7 +2,24 @@
 set -e
 
 apt-get update
-apt-get install -y python3-pip python3-venv git default-mysql-client
+apt-get install -y python3-pip python3-venv git default-mysql-client curl
+
+DB_HOST=$(curl -s -H "Metadata-Flavor: Google" \
+  http://metadata.google.internal/computeMetadata/v1/instance/attributes/DB_HOST)
+DB_USER=$(curl -s -H "Metadata-Flavor: Google" \
+  http://metadata.google.internal/computeMetadata/v1/instance/attributes/DB_USER)
+DB_PASSWORD=$(curl -s -H "Metadata-Flavor: Google" \
+  http://metadata.google.internal/computeMetadata/v1/instance/attributes/DB_PASSWORD)
+DB_NAME=$(curl -s -H "Metadata-Flavor: Google" \
+  http://metadata.google.internal/computeMetadata/v1/instance/attributes/DB_NAME)
+BUCKET_NAME=$(curl -s -H "Metadata-Flavor: Google" \
+  http://metadata.google.internal/computeMetadata/v1/instance/attributes/BUCKET_NAME)
+
+export DB_HOST
+export DB_USER
+export DB_PASSWORD
+export DB_NAME
+export BUCKET_NAME
 
 cd /root
 
